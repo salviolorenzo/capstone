6f41bab341fdf8ee3756eaf88e6e6bc8873ce59b
Lorenzo Salvio <salviolorenzo@gmail.com>
2019-01-05 18:34:55 -0500

    adds yelp key to config

--- src/Components/Tiles/Map.js
+++ src/Components/Tiles/Map.js
@@ -63,6 +63,9 @@ class MapContainer extends Component {
             <Marker
               key={index}
               name={item.name}
+              categories={item.category}
+              rating={item.avg_rating}
+              pricing={item.price}
               position={{
                 lat: parseFloat(item.location.latitude),
                 lng: parseFloat(item.location.longitude)
@@ -76,8 +79,11 @@ class MapContainer extends Component {
           visible={this.state.showingInfoWindow}
           onClose={this.onClose.bind(this)}
         >
-          <div>
+          <div className='infoWindow'>
             <h4>{this.state.selectedPlace.name}</h4>
+            <h6>{this.state.selectedPlace.categories}</h6>
+            <h5>{`Avg. Rating: ${this.state.selectedPlace.rating}/5`}</h5>
+            <h5>{`Pricing: ${this.state.selectedPlace.pricing}/4`}</h5>
           </div>
         </InfoWindow>
       </Map>
