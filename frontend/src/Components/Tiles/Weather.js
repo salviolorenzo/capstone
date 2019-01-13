780b6fa990e6bbf3a4d825b932282b556c5cd3a1
Lorenzo Salvio <salviolorenzo@gmail.com>
2019-01-13 16:41:11 -0500

    removes lazy loadign

--- frontend/src/Components/Tiles/Weather.js
+++ frontend/src/Components/Tiles/Weather.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 function Weather(props) {
   if (props) {
     return (
       <div className="tile weatherTile">
         <h3>Today's weather</h3>
-        <LazyLoadImage
-          src={props.icon}
-          className="weatherIcon"
-          alt="WeatherIcon"
-        />
+        <img src={props.icon} className="weatherIcon" alt="WeatherIcon" />
         <ul className="weatherList">
           {Object.values(props.weather).map((item, index) => {
             return <li key={index}>{item}</li>;
