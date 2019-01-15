c7ccd6d5263d1209888c3b013a25b59ff7bde14f
Lorenzo Salvio <salviolorenzo@gmail.com>
2019-01-15 14:22:59 -0500

    adds build to server

---
+++ server/public/precache-manifest.000bc8b3969f25c4cfe52b2814e00370.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a12f8b6da7d47a8f7bc",
+    "url": "/static/css/main.e4fa53a5.chunk.css"
+  },
+  {
+    "revision": "3a12f8b6da7d47a8f7bc",
+    "url": "/static/js/main.3a12f8b6.chunk.js"
+  },
+  {
+    "revision": "bfea61d0800149f4ffc7",
+    "url": "/static/js/1.bfea61d0.chunk.js"
+  },
+  {
+    "revision": "95c905c17933861cf65d",
+    "url": "/static/js/2.95c905c1.chunk.js"
+  },
+  {
+    "revision": "32c3bb5307854103d489",
+    "url": "/static/js/3.32c3bb53.chunk.js"
+  },
+  {
+    "revision": "7815fdf381ed42ccfa7e",
+    "url": "/static/js/4.7815fdf3.chunk.js"
+  },
+  {
+    "revision": "f8a249e7b2fccbe55b10",
+    "url": "/static/js/5.f8a249e7.chunk.js"
+  },
+  {
+    "revision": "38b5dc932412ef637031",
+    "url": "/static/js/6.38b5dc93.chunk.js"
+  },
+  {
+    "revision": "561ebc5b9f598ce7aba4",
+    "url": "/static/js/7.561ebc5b.chunk.js"
+  },
+  {
+    "revision": "1d199619d86979d3f1b8",
+    "url": "/static/js/8.1d199619.chunk.js"
+  },
+  {
+    "revision": "3083c677576dcc599fd4",
+    "url": "/static/js/9.3083c677.chunk.js"
+  },
+  {
+    "revision": "23e4184bba0e693d783f",
+    "url": "/static/js/10.23e4184b.chunk.js"
+  },
+  {
+    "revision": "6508693d6276b7b8af2c",
+    "url": "/static/css/11.f8f34164.chunk.css"
+  },
+  {
+    "revision": "6508693d6276b7b8af2c",
+    "url": "/static/js/11.6508693d.chunk.js"
+  },
+  {
+    "revision": "c6bbbf9fd579d1662c52",
+    "url": "/static/js/12.c6bbbf9f.chunk.js"
+  },
+  {
+    "revision": "fff5e27976da6fb5ac23",
+    "url": "/static/js/13.fff5e279.chunk.js"
+  },
+  {
+    "revision": "3c0745a56b5701607c23",
+    "url": "/static/js/14.3c0745a5.chunk.js"
+  },
+  {
+    "revision": "5b349fbef69cce81ed1a",
+    "url": "/static/js/runtime~main.5b349fbe.js"
+  },
+  {
+    "revision": "f04fc58f10ee0555d6728cbac90ba3c2",
+    "url": "/static/media/DSH_LOGO.f04fc58f.png"
+  },
+  {
+    "revision": "908a0081015f4b791b69e4d90b4796bc",
+    "url": "/static/media/rootBg.908a0081.jpeg"
+  },
+  {
+    "revision": "6efecfb272b52c9a8a475256519004f0",
+    "url": "/index.html"
+  }
+];
